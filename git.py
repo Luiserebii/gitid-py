a752a080332b6193bb178e0f6babaf9d61b8b508
Luiserebii <luis@serebii.io>
2019-10-17 19:19:39 -0400

    Attempt at fleshing out get_user_global, using Pythonic syntax and all

--- git.py
+++ git.py
@@ -1,7 +1,14 @@
+import subprocess
+
 class Git: 
 
     @staticmethod
-    def get_user_global: 
+    def get_user_global(): 
+        name = subprocess.check_output(["git", "config", "--global", "user.name"])
+        email = subprocess.check_output(["git", "config", "--global", "user.name"])
+        signing_key = subprocess.check_output(["git", "config", "--global", "user.signingkey"])
+        git_user = GitUser(name, email, signing_key)
+        return git_user;
 
 
     @staticmethod
