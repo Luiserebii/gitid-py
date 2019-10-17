09064c6c6a028632ca5f9dccd3ae015fdf093350
Luiserebii <luis@serebii.io>
2019-10-17 19:43:53 -0400

    Fiddling with encoding; it was a byte string, not a string, so tried decoding with 'utf-8', only to get: ============================ Luiserebii  luis@serebii.io  3B7E2D68E27CBBCF
    ============================
    exactly as output between those borders. Attempting some things

--- git.py
+++ git.py
@@ -1,16 +1,16 @@
 import subprocess
-import git_user
+from git_user import GitUser
 
 class Git: 
 
     @staticmethod
     def get_user_global(): 
-        name = subprocess.check_output(["git", "config", "--global", "user.name"])
-        email = subprocess.check_output(["git", "config", "--global", "user.name"])
-        signing_key = subprocess.check_output(["git", "config", "--global", "user.signingkey"])
+        name = subprocess.check_output(["git", "config", "--global", "user.name"]).decode(sys.stdout.encoding)
+        email = subprocess.check_output(["git", "config", "--global", "user.email"]).decode(sys.stdout.encoding)
+        signing_key = subprocess.check_output(["git", "config", "--global", "user.signingkey"]).decode(sys.stdout.encoding)
+
         git_user = GitUser(name, email, signing_key)
         return git_user;
-
 
     @staticmethod
     def get_user_local():
@@ -35,3 +35,8 @@ class Git:
     @staticmethod
     def build_revise_cmd():
         return
+
+def printUser(u):
+    print(u.name + " " + u.email + " " + u.signing_key)
+
+printUser(Git.get_user_global())
