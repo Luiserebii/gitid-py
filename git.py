c3fc12b6da5ee140abcbbdc58f338f073ab9d84d
Luiserebii <luis@serebii.io>
2019-10-17 18:59:15 -0400

    Adding .gitignore, starting git class

---
+++ git.py
@@ -0,0 +1,23 @@
+class Git: 
+
+    @staticmethod
+    def get_user_global: 
+
+
+    @staticmethod
+    def get_user_local:
+
+    @staticmethod
+    def set_user_global:
+
+    @staticmethod
+    def set_user_local: 
+
+    @staticmethod
+    def clone:
+
+    @staticmethod
+    def revise:
+
+    @staticmethod
+    def build_revise_cmd:
