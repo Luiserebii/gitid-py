0954061de05523d1ce25d65787b423ddbdfe8cfb
Luiserebii <luis@serebii.io>
2019-10-17 20:39:44 -0400

    Added get_user_local() and set_user_global(), using .format to stick variables in where they should be

--- git.py
+++ git.py
@@ -14,11 +14,25 @@ class Git:
 
     @staticmethod
     def get_user_local():
-        return
+        name = Util.exec(["git", "config", "--local", "user.name"])
+        email = Util.exec(["git", "config", "--local", "user.email"])
+        signing_key = Util.exec(["git", "config", "--local", "user.signingkey"])
+
+        git_user = GitUser(name, email, signing_key)
+        return git_user;
 
     @staticmethod
-    def set_user_global():
-        return
+    def set_user_global(git_user, prefix_cmd=None):
+        cmd = """git config --global user.name {} &&
+            git config --global user.email {}""".format(git_user.name, git_user.email)
+        if(git_user.signing_key != None): 
+            cmd += "git config --global user.signing_key {}".format(git_user.signing_key)
+        if(prefix_cmd != None):
+            cmd += prefix_cmd + ' && ' + cmd
+
+        # Finally after constructing the command, run
+        Util.exec(cmd)
+        return true;
 
     @staticmethod
     def set_user_local(): 
@@ -40,3 +54,4 @@ def printUser(u):
     print(u.name + " " + u.email + " " + u.signing_key)
 
 printUser(Git.get_user_global())
+printUser(Git.get_user_local())
--- util.py
+++ util.py
@@ -4,7 +4,6 @@ class Util:
 
     @staticmethod
     def exec(args, no_strip=False):
-
         out = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.decode(sys.stdout.encoding)
         if(not no_strip):
             out = out.strip();
